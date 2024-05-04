5ad21642fc2214566e1d93ec3563c00461f2c4a1
ali0083moi <ali0083moghadasi.mypc@gmai.com>
2024-05-04 15:51:15 +0330

    some method added to the GameMenuController.

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -7,7 +7,9 @@ public enum Controller {
     LOGIN_MENU_CONTROLLER(new LoginMenuController()),
     MAIN_MENU_CONTROLLER(new MainMenuController()),
     PROFILE_MENU_CONTROLLER(new ProfileMenuController()),
-    GAME_MENU_CONTROLLER(new GameMenuController());
+    GAME_MENU_CONTROLLER(new GameMenuController()),
+    PRE_GAME_MENU_CONTROLLER(new PregameController()),
+    ;
     private final AppController controller;
 
     Controller(AppController controller) {
--- src/Controller/PregameController.java
+++ src/Controller/PregameController.java
@@ -69,4 +69,8 @@ public class PregameController extends AppController {
     public void startGame() {
         
     }
+
+    public void addRandomCardsToHand() {
+
+    }
 }
